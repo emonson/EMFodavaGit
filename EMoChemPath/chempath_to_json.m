9102a161aa50a886438979df357d7f7edf35de0d
Eric E Monson <emonson@cs.duke.edu>
2014-02-25 12:28:14 -0500

    updated net.mat name

--- EMoChemPath/chempath_to_json.m
+++ EMoChemPath/chempath_to_json.m
@@ -2,12 +2,18 @@
 % for visualization with the interactive D3 ellipses & paths
 
 load('d_info.mat');
+% NOTE: lmks is a very large sparse matrix that we're not using in the vis right
+%   now...
+for ii=1:length(d_info),
+    d_info{ii} = rmfield(d_info{ii}, 'lmks');
+end
 savejson('',d_info,'ArrayToStruct',1,'ArrayIndent',0,'FileName','d_info.json','ForceRootName',0);
+fprintf('d_info done\n');
 
 clear all;
 
-load('netpoints.mat');
-savejson('',netpoints,'ArrayToStruct',1,'ArrayIndent',0,'FileName','netpoints.json','ForceRootName',0);
+load('net.mat');
+savejson('',net,'ArrayToStruct',1,'ArrayIndent',0,'FileName','netpoints.json','ForceRootName',0);
 
 clear all;
 
